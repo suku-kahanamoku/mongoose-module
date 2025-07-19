8680fa8eaf192a6513b046130ceda2628bc46619
Suche Kahanamoku <sukusovi@gmail.com>
2025-07-19 12:32:49 +0200

    Update

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['../src/module'],
-  myModule: {},
+  modules: ["../src/module"],
+  mongooseModule: {},
   devtools: { enabled: true },
-})
+});
--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -1,7 +1,5 @@
-import MyModule from '../../../src/module'
+import MongooseModule from "../../../src/module";
 
 export default defineNuxtConfig({
-  modules: [
-    MyModule,
-  ],
-})
+  modules: [MongooseModule],
+});
